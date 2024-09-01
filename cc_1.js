01fe10334e257d74a34bfd0c5c072119e41fb9b5
Fernanda <fernanda19@usf.edu>
2024-09-01 17:28:35 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,14 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive,typeof(isActive));
+
+
+//Task 2: Compound Data Types
+
+let products = ["Popcorn", "Chocolate", "Movie Tickets"];
+const productDetails = {
+    name: products; 
+    price: 29.99
+    InStock = "yes"
+
+} console.log(products, productDetails);
