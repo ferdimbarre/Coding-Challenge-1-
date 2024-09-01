775f4f940cf6eb7ae439cc570924e42e678bc41a
Fernanda <fernanda19@usf.edu>
2024-09-01 14:11:38 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1:  Variables and Data Types
+let employeeName = "Mary Poppins";
+const employeeID = 12;
+var isActive = true; 
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive,typeof(isActive));
