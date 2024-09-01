66450678e3ee46dde85634b6ef120f1a74552016
Fernanda <fernanda19@usf.edu>
2024-09-01 18:01:14 -0400

    Task 3 - Financial Calculation

--- cc_1.js
+++ cc_1.js
@@ -20,13 +20,19 @@ const productDetails = {
 
 // Task 3: Assignment Operators
 let accountBalance = 200;
+console.log(accountBalance); // = 200
+
 accountBalance += 10;
+console.log(accountBalance); // = 210
 
 accountBalance -= 200;
+console.log(accountBalance); // = 10
 
 accountBalance *= 10;
+console.log(accountBalance); // = 100
 
 accountBalance /= 2;
+console.log(accountBalance); // = 50
 
 accountBalance %= 6;
-
+console.log(accountBalance); // = 2
